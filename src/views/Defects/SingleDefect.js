168f1bde6821fa8ae41e7a46516631f0078fb5db
Lilian N <izzopod@gmail.com>
2020-12-20 13:11:11 -0600

    add edit form

--- src/views/Defects/SingleDefect.js
+++ src/views/Defects/SingleDefect.js
@@ -1,19 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useParams } from "react-router-dom";
+import { Row, Col, Button } from "reactstrap";
 
 import { fetchOneDefect, selectDefectById } from "../../redux-store/defectSlice";
+import { selectProjectById } from "redux-store/projectSlice";
 
 import Loading from "../../components/Loading";
 import DefectInfoDisplay from "../../components/Defects/DefectInfoDisplay";
-import { selectProjectById } from "redux-store/projectSlice";
+import EditDefectForm from "../../components/Defects/EditDefectForm";
 
 const SingleDefect = () => {
+  const id = useParams().id;
   const dispatch = useDispatch();
   const { getAccessTokenSilently } = useAuth0();
 
-  const id = useParams().id;
+  const defect = useSelector(state => selectDefectById(state, id));
+  const status = useSelector(({ defects }) => defects.status);
+  const error = useSelector(({ defects }) => defects.error);
+
+  const [formOpen, setFormOpen] = useState(false);
 
   useEffect(() => {
     getAccessTokenSilently()
@@ -24,10 +31,6 @@ const SingleDefect = () => {
       .then()
   }, [getAccessTokenSilently, dispatch])
 
-  const defect = useSelector(state => selectDefectById(state, id));
-  const status = useSelector(({ defects }) => defects.status);
-  const error = useSelector(({ defects }) => defects.error);
-
   let projectIdForDefect = defect ? defect.projectId : "";
   const project = useSelector(state => selectProjectById(state, projectIdForDefect));
 
@@ -35,14 +38,37 @@ const SingleDefect = () => {
 
   if (status == "failed") {
     return (
-      <div className="content">{error}</div>
+      <div className="content container">{error}</div>
     )
   }
 
-  if (defect) {
+  if (!!defect) {
     return (
-      <div className="content">
-        <DefectInfoDisplay defect={defect} projectTitle={projectTitle} />
+      <div className="content container">
+        <Row>
+          <Col className="d-flex justify-content-end">
+            <Button
+              color="info"
+              onClick={() => setFormOpen(true)}
+              size="sm"
+              style={{ height: 40 }}
+            >
+              Edit Defect
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <EditDefectForm
+              open={formOpen}
+              setOpen={setFormOpen}
+              defect={defect}
+              user={""}
+              projectTitle={projectTitle}
+            />
+            <DefectInfoDisplay defect={defect} projectTitle={projectTitle} />
+          </Col>
+        </Row>
       </div>
     )
   }
