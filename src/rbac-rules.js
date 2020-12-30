8b3ca8360ad5ee8f20c64723ffb4009e84a01a7b
Lilian N <izzopod@gmail.com>
2020-12-30 03:46:56 -0600

    add rbac modules

---
+++ src/rbac-rules.js
@@ -0,0 +1,25 @@
+const rules = {
+  "UNASSIGNED": {
+    static: [
+      "home-page:visit",
+      "sign-up:visit"
+    ]
+  },
+  "ADMIN": {
+    static: [
+      "admin-layout:view"
+    ]
+  },
+  "PROJECT LEAD": {
+    static: [
+      "lead-layout:view"
+    ]
+  },
+  "PROJECT MEMBER": {
+    static: [
+      "member-layout:view"
+    ]
+  }
+}
+
+export default rules;
