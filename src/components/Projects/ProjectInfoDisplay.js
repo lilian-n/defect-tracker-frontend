763748092fb7e2e124052d13661bf24bbd0f1c79
Lilian N <izzopod@gmail.com>
2020-12-30 03:51:57 -0600

    fix layout

--- src/components/Projects/ProjectInfoDisplay.js
+++ src/components/Projects/ProjectInfoDisplay.js
@@ -14,25 +14,29 @@ const ProjectInfoDisplay = ({ project }) => {
           </CardHeader>
 
           <CardBody>
-            <div className="typography-line">
-              <h6>Project Description</h6>
-              <p>{project.description}</p>
-            </div>
+            <Row>
+              <Col xs="12" sm="6" >
+                <h6>Project Description</h6>
+                <p>{project.description}</p>
+              </Col>
 
-            <div className="typography-line">
-              <h6>Start Date</h6>
-              <p>{project.startDate}</p>
-            </div>
+              <Col xs="12" sm="6">
+                <h6>Start Date</h6>
+                <p>{project.startDate}</p>
+              </Col>
+            </Row>
 
-            <div className="typography-line">
-              <h6>Target End Date</h6>
-              <p>{project.targetEndDate}</p>
-            </div>
+            <Row>
+              <Col xs="12" sm="6">
+                <h6>Target End Date</h6>
+                <p>{project.targetEndDate}</p>
+              </Col>
 
-            <div className="typography-line">
-              <h6>Actual End Date</h6>
-              <p>{actualEndDate}</p>
-            </div>
+              <Col xs="12" sm="6">
+                <h6>Actual End Date</h6>
+                <p>{actualEndDate}</p>
+              </Col>
+            </Row>
           </CardBody>
         </Card>
       </Col>
