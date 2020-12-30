0b4b4b3535be67cebad849048f67d2062c373061
Lilian N <izzopod@gmail.com>
2020-12-30 03:50:31 -0600

    remove unused routes

--- src/routes.js
+++ src/routes.js
@@ -17,11 +17,6 @@
 
 */
 import Dashboard from "views/Dashboard";
-import Notifications from "views/Notifications";
-import Icons from "views/Icons";
-import Typography from "views/Typography";
-import TableList from "views/Tables";
-import UserPage from "views/User";
 import ProjectList from "views/Projects/ProjectList";
 import SingleProject from "views/Projects/SingleProject";
 import AdminDefects from "views/Defects/AdminDefects";
@@ -36,41 +31,6 @@ export const dashboardRoutes = [
     component: Dashboard,
     layout: "/admin",
   },
-  // {
-  //   path: "/icons",
-  //   name: "Icons",
-  //   icon: "nc-icon nc-diamond",
-  //   component: Icons,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/notifications",
-  //   name: "Notifications",
-  //   icon: "nc-icon nc-bell-55",
-  //   component: Notifications,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/user-page",
-  //   name: "User Profile",
-  //   icon: "nc-icon nc-single-02",
-  //   component: UserPage,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/tables",
-  //   name: "Table List",
-  //   icon: "nc-icon nc-tile-56",
-  //   component: TableList,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/typography",
-  //   name: "Typography",
-  //   icon: "nc-icon nc-caps-small",
-  //   component: Typography,
-  //   layout: "/admin",
-  // },
   {
     path: "/projects",
     name: "Projects",
