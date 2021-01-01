e148468218c5eac20203ed9ab5bde731837875c4
Lilian N <izzopod@gmail.com>
2021-01-01 13:25:28 -0600

    add role base redirects

--- src/auth/ProtectedRoute.js
+++ src/auth/ProtectedRoute.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 
 import Loading from "../components/Loading"
 
-const ProtectedRoute = ({ component, ...args }) => (
+const ProtectedRoute = ({ component: Component, role, ...args }) => (
   <Route
-    component={withAuthenticationRequired(component, {
-      onRedirecting: () => <Loading />
-    })}
+    render={(props) => {
+      if (role === "UNASSIGNED") {
+        return <Redirect to={{ pathname: "/signup", state: { from: props.location } }} />
+      }
+
+      return <Component {...props} />
+    }}
     {...args}
   />
 );
 
-export default ProtectedRoute;
-
+export default withAuthenticationRequired(ProtectedRoute, {
+  onRedirecting: () => <Loading />
+})
