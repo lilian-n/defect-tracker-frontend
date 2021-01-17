1d1146f06ff7cbdeee5034c9d6a2325141fb162c
Lilian N <izzopod@gmail.com>
2021-01-17 10:45:49 -0600

    rename BrowserRouter

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -31,12 +31,12 @@ import store from "./redux-store/store";
 import App from "./App";
 
 ReactDOM.render(
-  <Router>
+  <BrowserRouter>
     <Auth0ProviderWithHistory>
       <Provider store={store}>
         <App />
       </Provider>
     </Auth0ProviderWithHistory>
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
