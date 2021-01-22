aa9dd03fecd0a91c2d0e8694654faf0b1cb0e3cc
Lilian N <izzopod@gmail.com>
2021-01-22 03:10:44 -0600

    remove useState

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 import PerfectScrollbar from "perfect-scrollbar";
 import { Switch, useHistory } from "react-router-dom";
